d50e1bd3f8e833bd7a6d0a74ec8ccf0e907376e6
purbanus <purbanus@gmail.com>
2022-08-30 20:23:06 +0200

    Bestsum gemaakt en de andere packages gerenamed

---
+++ pu.dynamic-programming-youtube-tutorial/src/main/java/pu/dynamic_programming/example5/bestsum/BestSum.java
@@ -0,0 +1,75 @@
+package pu.dynamic_programming.example5.bestsum;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BestSum
+{
+
+public static void main( String [] args )
+{
+	new BestSum().run();
+}
+
+private void run()
+{
+
+	bestSum( 7, List.of( 5, 3, 4, 7 ) );
+	bestSum( 8, List.of( 2, 3, 5 ) );
+	bestSum( 8, List.of( 1, 4, 5 ) );
+	bestSum( 100, List.of( 1, 2, 5, 25 ) );
+
+}
+private void log( Object aThing )
+{
+	System.out.println( aThing );
+}
+
+private void bestSum( int aTarget, List<Integer> aParts )
+{
+	List<Integer> bestSums = bestSum( aTarget, aParts, new HashMap<>() );
+	if ( bestSums != null )
+	{
+		Collections.reverse( bestSums );
+	}
+	log( "Making " + aTarget + " from " + aParts + (bestSums != null ? " can be done by " + bestSums : " cannot be done" ) );
+}
+
+private List<Integer> bestSum( int aTarget, List<Integer> aParts, Map<Integer, List<Integer>> aMemo )
+{
+	if ( aMemo.containsKey( aTarget ) )
+	{
+		return aMemo.get( aTarget );
+	}
+	if ( aTarget == 0 )
+	{
+		return new ArrayList<>();
+	}
+	if ( aTarget < 0 )
+	{
+		return null;
+	}
+	List<Integer> bestSum = null;
+	for ( int part : aParts )
+	{
+		int newTarget = aTarget - part;
+	
+		List<Integer> newBestSum = bestSum( newTarget, aParts, aMemo );
+		if ( newBestSum != null )
+		{
+			newBestSum = new ArrayList<>( newBestSum );
+			newBestSum.add( part );
+			if ( bestSum == null || newBestSum.size() < bestSum.size() )
+			{
+				bestSum = newBestSum;
+			}
+		}
+	}
+	aMemo.put( aTarget, bestSum );
+	return aMemo.get( aTarget );
+}
+
+}
